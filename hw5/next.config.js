376aee1b56a2c3d1fd75be19335e8b32cade464a
陳竑齊 <chenjustin824@gmail.com>
2025-11-08 16:46:18 +0800

    test5

--- hw5/next.config.js
+++ hw5/next.config.js
@@ -1,13 +1,8 @@
-  /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXTAUTH_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : 'http://localhost:3000',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
   },
 };
 
 module.exports = nextConfig;
-
-
